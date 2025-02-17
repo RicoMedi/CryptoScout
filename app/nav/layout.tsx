e2c929fbc058100b7435647f564da4622ced3eb1
Ricardo Medina <r.medina2119@gmail.com>
2025-02-16 20:39:31 -0800

    styling navigation

--- app/nav/layout.tsx
+++ app/nav/layout.tsx
@@ -1,6 +1,6 @@
 export default function Nav() {
   return (
-    <div className="flex">
+    <div className="flex border-b-2 border-gray-300 w-full justify-center items-center  p-10">
       <h1>Crypto</h1>
     </div>
   );
